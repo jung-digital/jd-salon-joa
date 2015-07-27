6f4813345b15747451abeb3115d35715a09aede7
joshjung <joshua.p.jung@gmail.com>
2015-07-27 15:54:47 -0500

    Making the background images dynamic and editable through the CMS.

--- routes/index.js
+++ routes/index.js
@@ -13,29 +13,34 @@ var routes = {
 
 // Setup Route Bindings
 exports = module.exports = function(app) {
-	app.get('/', routeWrap(routes.views.index));
-	app.get('/gallery', routeWrap(routes.views.gallery));
-	app.all('/services', routeWrap(routes.views.services));
-  app.all('/testimonials', routeWrap(routes.views.testimonials));
+	app.get('/', routeWrap(routes.views.index, 'index'));
+	app.get('/gallery', routeWrap(routes.views.gallery, 'gallery'));
+	app.all('/services', routeWrap(routes.views.services, 'services'));
+  app.all('/testimonials', routeWrap(routes.views.testimonials, 'testimonials'));
 };
 
-function routeWrap (routeFunc)
+function routeWrap (routeFunc, pageName)
 {
   return function (req, res)
   {
     var view = new keystone.View(req, res);
 
     keystone.list('Link').model.find().exec(function (err, links) {
-      
+      res.locals.links = map(links, 'name');
 
-      var linksByName = {};
-      links.forEach(function (l) {linksByName[l.name]=l;});
+      keystone.list('Image').model.find().exec(function (err, images) {
+        res.locals.images = map(images, 'name');
 
-      console.log(linksByName);
-
-      res.locals.links = linksByName;
-
-      routeFunc(req, res, view);  
+        res.locals.dynamicCSS = 'html {background-image:url(' + res.locals.images[pageName +'_bg'].image.url + ');}';
+        
+        routeFunc(req, res, view);  
+      });
     });
   };
+}
+
+function map(list, field) {
+  var _map = {};
+  list.forEach(function (i) {_map[i[field]] = i;});
+  return _map;
 }
